40022ce196c4ebaa5bdccd4898cf6fc96fba8b0b
adityasharma2020 <adityasharma.1.tech@gmail.com>
2022-03-21 17:24:07 +0530

    started

---
+++ app.js
@@ -0,0 +1,36 @@
+const age = parseInt(prompt("what is your age ? "));
+// console.log(age)
+if(isNaN(age)){
+    console.log("wrie a number");
+}else if(age <18){
+    console.log("you are too young");
+}else if(age >18 && age <50){
+    console.lot("you can drink");
+}else
+
+
+
+// const calculator ={
+
+//     add : function(a,b){
+//       alert(a+b);
+//     },
+//     minus : function(a,b){
+//       alert(a-b);
+//     },
+//     mult: function(a,b){
+//       alert(a*b);
+//     },
+//     divide: function(a,b){
+//       alert(a/b);
+//     },
+//     power: function(a,b){
+//       alert(a**b);
+//     }
+// }; 
+
+// console.log(calculator.add(10,2));
+// calculator.minus(10,2);
+// calculator.mult(10,2);
+// calculator.divide(10,2);
+// calculator.power(10,2);
