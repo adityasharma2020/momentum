1ca6df8f041f53b9a5a51d7948af61731432227f
adityasharma2020 <adityasharma.1.tech@gmail.com>
2022-03-22 23:29:42 +0530

    print greeting

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 const loginForm = document.querySelector('#login-form');
 const loginInput = loginForm.querySelector('input');
-const loginButton = loginForm.querySelector('button');
 
-function onLoginBtnClick() {
+//we write the less useful variales capital,that ontains strings generally 
+const HIDDEN_CLASSNAME = 'hidden';
+const greeting = document.querySelector('#greeting');
+
+//this happens when we press enter  or we click the button
+function onLoginSubmit(event) {
+  //this stops the default behaviour any event from happening.
+  event.preventDefault();
   const userName = loginInput.value;
+  console.log(userName)
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  greeting.innerText = `hello ${userName}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 
-  if (userName === '') {
-    alert('please weite your name');
-  } else if (userName.length > 15) {
-    alert('your name is too long');
-  }
-
-  console.log('hello', loginInput.userName);
+  //js will give the event that just happened as the first argument.
+  // console.log(event);
 }
 
-loginButton.addEventListener('click', onLoginBtnClick);
+function handleLinkClick(event) {
+  event.preventDefault();
+  console.dir(event);
+}
+
+loginForm.addEventListener('submit', onLoginSubmit);
