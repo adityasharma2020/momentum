1d52c41a3f74e1cc1ccb1cc209f91f8fb9dd64e5
adityasharma2020 <adityasharma.1.tech@gmail.com>
2022-03-24 09:24:13 +0530

    background

---
+++ js/backgroud.js
@@ -0,0 +1,7 @@
+const images = ['0.jpeg', '1.jpeg', '2.jpeg'];
+
+const chosenImages = images[Math.floor(Math.random() * images.length)];
+
+console.log(chosenImages);
+const bgImage = document.createElement('img');
+console.log(bgImage);
